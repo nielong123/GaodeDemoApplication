46a5150860b172bcf73cb4e98f3d4a730c05e257
nielong <13469986047@163.com>
2016-11-22 14:59:15 +0800

    增加了地图轨迹播放功能

--- app/src/main/java/gaode/trajectory/TrajectoryActivity.java
+++ app/src/main/java/gaode/trajectory/TrajectoryActivity.java
@@ -2,16 +2,20 @@ package gaode.trajectory;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
 import com.amap.api.maps.UiSettings;
 import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.Polyline;
 import com.google.gson.Gson;
+import com.kyleduo.switchbutton.SwitchButton;
 
 import bean.Bean;
 import gaodedemo.nl.org.gaodedemoapplication.R;
@@ -21,7 +25,7 @@ import tools.ReadStringUtil;
  * Created by nl on 2016/11/21.
  */
 
-public final class TrajectoryActivity extends Activity implements View.OnClickListener, AMap.InfoWindowAdapter {
+public final class TrajectoryActivity extends Activity implements View.OnClickListener, AMap.InfoWindowAdapter, SeekBar.OnSeekBarChangeListener,CompoundButton.OnCheckedChangeListener {
 
     private Button btn1;
     private Button btn2;
@@ -29,6 +33,19 @@ public final class TrajectoryActivity extends Activity implements View.OnClickLi
     private Bean bean;
     private MapView mapview;
     private AMap aMap;
+    private ImageView play_image;
+    private RelativeLayout play_root;
+    private SeekBar seekbar;
+    private SwitchButton switchButton;
+
+
+    private Polyline mPolyline;
+
+    private Marker mStartMarker;            //起点
+
+    private Marker mEndMarker;              //终点
+
+    private Marker mMoveMarker;             //...
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,6 +89,14 @@ public final class TrajectoryActivity extends Activity implements View.OnClickLi
         mapview.onCreate(savedInstanceState);
         aMap = mapview.getMap();
         UiSettings uiSettings = aMap.getUiSettings();
+        play_image = (ImageView) findViewById(R.id.play_image);
+        play_image.setOnClickListener(this);
+        play_root = (RelativeLayout) findViewById(R.id.play_root);
+        play_root.setOnClickListener(this);
+        seekbar = (SeekBar) findViewById(R.id.seekbar);
+        seekbar.setOnClickListener(this);
+        switchButton = (SwitchButton) findViewById(R.id.switchButton);
+        switchButton.setOnCheckedChangeListener(this);
     }
 
     @Override
@@ -87,6 +112,10 @@ public final class TrajectoryActivity extends Activity implements View.OnClickLi
         bean = new Gson().fromJson(str, Bean.class);
     }
 
+    /**************
+     * Gaode Marker
+     *************/
+
     @Override
     public View getInfoWindow(Marker marker) {
         return null;
@@ -95,5 +124,34 @@ public final class TrajectoryActivity extends Activity implements View.OnClickLi
     @Override
     public View getInfoContents(Marker marker) {
         return null;
+    }
+
+    /******************
+     * seekbar
+     ***************/
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    /****
+     *  switchButton
+     * @param buttonView
+     * @param isChecked
+     */
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
     }
 }
